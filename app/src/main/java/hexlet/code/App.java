b4963be5d92124a53ccd67139c6d465505054942
Ekaterina Avdeenko <Katherini@yandex.ru>
2025-05-01 12:42:55 +0300

    feat: Implement help message using Picocli

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,31 @@
 package hexlet.code;
 
-public class App {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Parameters;
+
+import java.util.concurrent.Callable;
+// import hexlet.code.Differ;
+
+@Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0",
+        description = "Compares two configuration files and shows a difference.")
+public class App implements Callable<Integer> {
+
+    // @Parameters(index = "0", description = "Path to the first file")
+    // private String filePath1;
+
+    // @Parameters(index = "1", description = "Path to the second file")
+    // private String filePath2;
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
+    }
+
+    @Override
+    public Integer call() throws Exception {
+        // String diff = Differ.generate(filePath1, filePath2);
+        System.out.println("Hello picocli");
+        return 0;
     }
 }
