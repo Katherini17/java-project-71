a2fbee5fecdea37ee468a2c03f4cf4ab6fe346ef
Ekaterina Avdeenko <Katherini@yandex.ru>
2025-05-02 13:32:59 +0300

    feat: Implement command-line format and parametrs using Picocli
    Add filepath1 and filepath2 parametrs for paths to files and --format option using Picocli annotations.

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -2,6 +2,7 @@ package hexlet.code;
 
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
 import java.util.concurrent.Callable;
@@ -10,12 +11,14 @@ import java.util.concurrent.Callable;
 @Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0",
         description = "Compares two configuration files and shows a difference.")
 public class App implements Callable<Integer> {
+    @Option(names = {"-f", "--format"}, description = "output format", defaultValue = "stylish")
+    private String format = "stylish";
 
-    // @Parameters(index = "0", description = "Path to the first file")
-    // private String filePath1;
+    @Parameters(index = "0", description = "path to the first file")
+    private String filepath1;
 
-    // @Parameters(index = "1", description = "Path to the second file")
-    // private String filePath2;
+    @Parameters(index = "1", description = "path to the second file")
+    private String filepath2;
 
     public static void main(String[] args) {
         int exitCode = new CommandLine(new App()).execute(args);
