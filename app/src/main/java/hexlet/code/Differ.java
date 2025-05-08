969b9f87bc6dceb94886ad1cd4cfe7055b153b05
Ekaterina Avdeenko <Katherini@yandex.ru>
2025-05-08 21:53:10 +0300

    feat: Implement Differ class for comparing data structures

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -1,5 +1,42 @@
 package hexlet.code;
 
-public class DiffFinder {
-    
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static hexlet.code.Parser.getData;
+
+public class Differ {
+    public static String generate(String filePath1, String filePath2) throws Exception {
+        Map<String, Object> data1 = getData(filePath1);
+        Map<String, Object> data2 = getData(filePath2);
+
+        Set<String> keys = new HashSet<>(data1.keySet());
+        keys.addAll(data2.keySet());
+
+        String diff = keys.stream()
+                .sorted()
+                .map(key -> {
+                    String keyPart = String.format(" %s: ", key);
+
+                    String valueOfData1 = String.valueOf(data1.get(key));
+                    String valueOfData2 = String.valueOf(data2.get(key));
+
+                    if (!data1.containsKey(key)) {
+                        return String.format("  +%s%s", keyPart, valueOfData2);
+                    } else if (!data2.containsKey(key)) {
+                        return String.format("  -%s%s", keyPart, valueOfData1);
+                    }
+
+                    if (valueOfData1.equals(valueOfData2)) {
+                        return String.format("   %s%s", keyPart, valueOfData1);
+                    }
+
+                    return String.format("  -%s%s%n  +%s%s", keyPart, valueOfData1, keyPart, valueOfData2);
+                })
+                .collect(Collectors.joining("\n"));
+
+        return String.format("{%n%s%n}", diff);
+    }
 }
