c75f21aac5191110d5c4342266c9111f3b5a493f
Ekaterina Avdeenko <Katherini@yandex.ru>
2025-05-10 16:47:42 +0300

    feat: Add Sonarcube Cloud

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,17 @@
 plugins {
     id("java")
     id("com.github.ben-manes.versions") version "0.52.0"
+    id("org.sonarqube") version "6.0.1.5171"
     application
     checkstyle
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "Katherini17_java-project-71")
+        property("sonar.organization", "katherini-17-projects")
+        property("sonar.host.url", "https://sonarcloud.io")
+    }
 }
 
 application {
