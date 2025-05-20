b0eb9586a45c911836bc4df0ab4d55db427d9abb
Ekaterina Avdeenko <Katherini@yandex.ru>
2025-05-20 21:54:32 +0300

    feat: Add overloaded Differ.generate method for two parameters

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -17,4 +17,8 @@ public class Differ {
         return formattedDiff;
     }
 
+    public static String generate(String filePath1, String filePath2) throws Exception {
+        return generate(filePath1, filePath2, "stylish");
+    }
+
 }
